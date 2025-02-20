6e49cfdfbf1010d8c5c3da60b41acc7233cf2c30
zangmiao <starzmisgod@gmail.com>
2025-02-20 19:36:23 +0800

    table opt

--- src/components/task/index.jsx
+++ src/components/task/index.jsx
@@ -769,7 +769,7 @@ function Row(props) {
 
     return (
         <React.Fragment>
-            <TableRow sx={{ minWidth: 1024 }}>
+            <TableRow sx={{ minWidth: 1240 }}>
                 <TableCell>
                     <IconButton
                         aria-label="expand row"
@@ -795,16 +795,28 @@ function Row(props) {
                         </div>
                     </Tooltip>
                 </TableCell>
-                <TableCell align="right">
-                    <div>{t('创建时间')}：{row.create_time > 0 ? (new Date(row.create_time * 1000).toLocaleTimeString('zh-CN', { month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false })) : ''} </div>
-                    <div>{t('最后一次运行时间')}：{row.task_info.last_run_time > 0 ? (new Date(row.task_info.last_run_time * 1000).toLocaleTimeString('zh-CN', { month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false })) : ''} </div>
+                <TableCell component="th" scope="row">
+                    <div>{t('任务创建时间')}：{row.create_time > 0 ? (new Date(row.create_time * 1000).toLocaleTimeString('zh-CN', { month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false })) : ''} </div>
+                    <div>{t('最后运行时间')}：{row.task_info.last_run_time > 0 ? (new Date(row.task_info.last_run_time * 1000).toLocaleTimeString('zh-CN', { month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false })) : ''} </div>
                     <div>{t('下一次运行时间')}：{row.task_info.next_run_time > 0 ? (new Date(row.task_info.next_run_time * 1000).toLocaleTimeString('zh-CN', { month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false })) : ''} </div>
                     <div>{t('运行类型')}：{row.task_info.run_type} </div>
-                    <div>{t('是否不需要检查')}：{row.original.no_check ? t('是') : t('否')}</div>
+                </TableCell>
+                <TableCell align="right">
+                    <div>{t('是否需要检查')}：{!row.original.no_check ? t('是') : t('否')}</div>
+                    {
+                        !row.original.no_check ? (
+                            <>
+                                <div>{t('检查方式')}：{row.original.ffmpeg_check ? t('ffmpeg慢速检查') : t('http快速检查')} </div>
+                                {
+                                    !row.original.ffmpeg_check ? (
+                                        <div>{t('如果非http链接则跳过')}：{row.original.not_http_skip ? t('是') : t('否')} </div>
+                                    ):''
+                                }
+                            </>
+                        ):''
+                    }
                     <div>{t('是否需要排序')}：{row.original.sort ? t('是') : t('否')} </div>
                     <div>{t('是否需要去掉频道多余字符')}：{row.original.rename ? t('是') : t('否')} </div>
-                    <div>{t('是否强制使用ffmpeg检查')}：{row.original.ffmpeg_check ? t('是') : t('否')} </div>
-                    <div>{t('如果不是http链接则跳过')}：{row.original.not_http_skip ? t('是') : t('否')} </div>
                     <div>{t('相同名称保存条数')}：{row.original.same_save_num > 0 ? row.original.same_save_num : t('保存全部')} </div>
                 </TableCell>
             </TableRow>
@@ -1261,7 +1273,7 @@ export default function TaskList(props) {
                                 <div>{t('当前设置的【后台检查server域名】为')}：{privateHost}</div>
                             ) : ''
                         }
-                        <Paper sx={{ width: '1024px', overflow: 'hidden' }}>
+                        <Paper sx={{ width: '1240px', overflow: 'hidden' }}>
                             <TableContainer>
                                 <Table aria-label="simple table">
                                     <TableHead>
@@ -1269,7 +1281,8 @@ export default function TaskList(props) {
                                             <TableCell></TableCell>
                                             <TableCell>任务id</TableCell>
                                             <TableCell>输出文件</TableCell>
-                                            <TableCell align="right">其他</TableCell>
+                                            <TableCell>运行时间</TableCell>
+                                            <TableCell align="right">任务信息</TableCell>
                                         </TableRow>
                                     </TableHead>
                                     <TableBody>
